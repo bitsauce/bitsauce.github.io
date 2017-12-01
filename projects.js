af94fe11500cbbc3cfe6b2a3623a3376390f56a5
Marcus <marcus.loo.vergara@gmail.com>
2017-11-30 18:49:07 -0800

    Updated some project descriptions

--- projects.js
+++ projects.js
@@ -39,7 +39,7 @@ var projects = {
         "title": "Tank AI",
         "description": "Framework for an AI competition for the game development student organization <a href=\"https://www.facebook.com/groups/1498086230434830/\">Abakus GameDev</a>. \
                         The game features top-down tank control gameplay, and the goal of the game is to be the last man standing. \
-                        Commands are issued using a socket-based solution, and the game is written in Java.",
+                        Commands are issued using a socket-based solution, and the game is written in Java with <a href=\"https://libgdx.badlogicgames.com/\">libGDX</a>.",
         "url": "https://github.com/bitsauce/AICompo",
         "images": ["https://i.imgur.com/iv5ve9E.png", "https://i.imgur.com/asa4NiR.png", "https://i.imgur.com/3IgwNC8.png"],
         "icon": "fa-gamepad",
@@ -57,7 +57,9 @@ var projects = {
     },
     "holmgang": {
         "title": "Holmgang (Global Game Jam 16)",
-        "description": "<i>Settle your disputes through Holmgang. But pay heed to the rituals, or die like a Niding!</i> Holmgang is a local 1v1 2D fighting game. In the game, rituals act as gameplay modifiers throughout the fight. I was primary gameplay programmer in the 4-man team.",
+        "description": "<i>Settle your disputes through Holmgang. But pay heed to the rituals, or die like a Niding!</i> \
+                        Holmgang is a local 1v1 2D fighting game. In the game, rituals act as gameplay modifiers throughout the fight. \
+                        I was primary gameplay programmer in the 4-man team. Made with <a href=\"https://www.yoyogames.com/\">Game Maker Studio</a>",
         "url": "https://github.com/bitsauce/GGJ16",
         "images": ["images/holmgang_1.png", "images/holmgang_2.png", "images/holmgang_3.png"],
         "icon": "fa-gamepad",
@@ -65,7 +67,9 @@ var projects = {
     },
     "grabster": {
         "title": "Grabster",
-        "description": "Summer job working as a backend and navigation programmer for Grabster's Android app (<a href=\"https://play.google.com/store/apps/details?id=no.grabster.android\">play store link</a>) made with React-Native. \
+        "description": "Summer job working as a backend and navigation programmer for Grabster's Android app \
+                        (<a href=\"https://play.google.com/store/apps/details?id=no.grabster.android\">play store link</a>) \
+                        made with <a href=\"https://facebook.github.io/react-native/\">React Native</a>. \
                         Grabster is an app which makes it easy to buy and sell homemade food. Any time you're hungry, you can just pick \
                         up your phone and see what's cooking in your neighborhood. ",
         "url": "http://grabster.no/#/en?_k=ruj3ro",
@@ -75,7 +79,9 @@ var projects = {
     },
     "computer-vision": {
         "title": "Autonomous Vehicle Perception",
-        "description": "Trained a region-based deep convolutional network (R-CNN) on the udacity dataset. Detects all of these 5 classes: cars, trucks, pedestrians, traffic lights and bikers with moderate accuracy.",
+        "description": "Trained a region-based deep convolutional network (R-CNN) on the udacity dataset. \
+                        Detects all of these 5 classes: cars, trucks, pedestrians, traffic lights and bikers with moderate accuracy. \
+                        Written in Python 3 using <a href=\"http://caffe.berkeleyvision.org/tutorial/interfaces.html\">pycaffe</a> (port of Caffe - a Deep Learning Framework)",
         "url": "https://github.com/bitsauce/Computer_Vision_Project",
         "images": ["https://i.imgur.com/xiUrNOe.png", "https://i.imgur.com/Ez367bd.png", "https://i.imgur.com/jMw3OMf.png"],
         "icon": "fa-book",
@@ -83,7 +89,7 @@ var projects = {
     },
     "visual-computing": {
         "title": "Visual Computing Fundamentals",
-        "description": "Simple model-view-projection rendering in 3D using OpenGL 4.3. Supports traditional WASD and mouse camera controls; WASD moves the camera relative to its Z-axis, and the mouse rotates the camera. Shapes can be selected and moved from one cell to another, applying smooth movement between start and destination.",
+        "description": "Simple model-view-projection rendering in 3D using C++ with OpenGL 4.3. Supports traditional WASD and mouse camera controls; WASD moves the camera relative to its Z-axis, and the mouse rotates the camera. Shapes can be selected and moved from one cell to another, applying smooth movement between start and destination.",
         "url": "https://github.com/bitsauce/Visual_Computing_Fundementals",
         "images": ["https://i.imgur.com/9QkB6kg.png"],
         "icon": "fa-book",
@@ -91,7 +97,7 @@ var projects = {
     },
     "compiler-construction": {
         "title": "Compiler Construction",
-        "description": "Created a basic compiler for the Very Simple Language (VSL) programming language. Does parsing, tokenizing and generates assembly code for the program. Some features include: lexical scoping, function calls, recursive function calls, branching, while-loops and return statements.",
+        "description": "Created a basic compiler for the Very Simple Language (VSL) programming language. Does parsing, tokenizing and generates assembly code for the program. Some features include: lexical scoping, function calls, recursive function calls, branching, while-loops and return statements. Written in C.",
         "url": "",
         "images": ["https://i.imgur.com/mAnSEpH.png"],
         "icon": "fa-book",
