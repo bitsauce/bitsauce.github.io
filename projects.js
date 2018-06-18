6de0038a1aac79edf07c8ae4757309af63339d3d
Marcus Loo Vergara <marcus.loo.vergara@gmail.com>
2018-06-18 16:41:31 -0700

    Added ML projects

--- projects.js
+++ projects.js
@@ -67,18 +67,34 @@ var projectTabs = {
         },
     },
     "Machine Learning": {
-        /*"plant-cloud": {
-            "title": "Learning on 3D Data",
-            "description": "Evaluated various deep learning models for classifying and doing segmentation of 3D point cloud data \
-                            of plants as part of research done by Salk.",
+        "keypoint-rcnn": {
+            "title": "Keypoint R-CNN",
+            "description": "Extended the Mask R-CNN model to detect human keypoint in images of people as part of the course <a href=\"https://cseweb.ucsd.edu/classes/sp18/cse252C-a/\">CSE 252C - Selected Topics in Vision and Learning</a> at UCSD. \
+                            By adding a keypoint prediction head parallel to the bounding box, classification and mask heads, this model predicts 17 different probability masks (one for each joint of the person,) \
+                            denoting the probability of finding a given joint type on a particular location in the image. The model was trained on the MS COCO human keypoint dataset.",
+            "url": "https://github.com/bitsauce/Keypoint_RCNN",
+            "images": ["images/keypoint-rcnn/model_overview.png",
+                       "images/keypoint-rcnn/gt_example.png",
+                       "images/keypoint-rcnn/predicted_kps_examples.png",
+                       "images/keypoint-rcnn/prediction_kp_heatmap_2063.png",
+                       "images/keypoint-rcnn/model_heads.png"]
+        },
+        "deep-learning-3d": {
+            "title": "Deep Learning on 3D Data",
+            "description": "Evaluated PointNet, PointNet++ and ShapePFCN for segmenting point cloud data \
+                            of plants in various stages of growth and various environments. Point clouds from the laser scanner \
+                            would often exceed 1 million points, so we experimented with various data preprocessing and architectural \
+                            changes to support these point clouds.",
             "url": "",
-            "images": ["images/plant-cloud/image_0.png"]
-        },*/
+            "images": ["images/deep-learning-3d/plant_example.png",
+                       "images/deep-learning-3d/plant_segmentations.png",
+                       "images/deep-learning-3d/plant_shapepfcn.png"]
+        },
         "image-captioning": {
             "title": "Image Captioning",
-            "description": "As part of the course <a href=\"https://cse.ucsd.edu/undergraduate/cse-190-topics-computer-science-and-engineering\">CSE 190</a> \
-                            on Neural Networks, we created a deep neural network for automatic captioning of images \
-                            using a convolutional neural network (CNN) to generate features, passing the features through \
+            "description": "As part of the course <a href=\"https://cse.ucsd.edu/undergraduate/cse-190-topics-computer-science-and-engineering\">CSE 190 - Neural Networks</a>, \
+                            we created a deep neural network for automatic captioning of images. \
+                            The network uses a convolutional neural network (CNN) to generate features, passing the features through \
                             a recurrent nerual network (RNN) to generate captions for the images. \
                             The network was trained on the flickr8k dataset.",
             "url": "",
@@ -89,7 +105,7 @@ var projectTabs = {
         },
         "computer-vision": {
             "title": "Autonomous Vehicle Perception",
-            "description": "Trained a region-based deep convolutional network (R-CNN) on the udacity dataset. \
+            "description": "Trained Faster R-CNN on the udacity dataset for my project in <a href=\"https://cse.ucsd.edu/undergraduate/cse-190-topics-computer-science-and-engineering\">TDT4265 - Computer Visison</a>. \
                             Detects all of these 5 classes: cars, trucks, pedestrians, traffic lights and bikers with moderate accuracy. \
                             Written in Python 3 using <a href=\"http://caffe.berkeleyvision.org/tutorial/interfaces.html\">pycaffe</a> -- a port of the deep learning framework, <a href=\"http://caffe.berkeleyvision.org/\">Caffe</a>.",
             "url": "https://github.com/bitsauce/Computer_Vision_Project",
